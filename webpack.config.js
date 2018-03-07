0eda634c9b0ab8b01361774afccffec1b5147ff6
sso66 <sun.stephen.oo@gmail.com>
2018-03-07 10:09:53 -0500

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// webpack.config.js
+console.info('Mounting webpack.config.js...');
+
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  // template: __dirname + '/public/templates/index.html',
+  // filename: 'index.html',
+  // inject: 'body',
+// });
+
+module.exports = {
+  entry: __dirname + '/public/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  }, 
+  watch: true,
+  // module: {
+    // loaders: [
+      // {
+        // test: /\.jshint$/,
+        // exclude: /node_modules/,
+        // loader: 'jshint-loader',
+        // query: {
+          // cacheDirectory: true,  
+          // presets: ['react', 'es2015']
+        // }
+      // },  
+      // {
+        // test: [/\.js$/, /\.es6$/],
+        // exclude: /node_modules/,
+        // loader: 'babel-loader',
+        // query: {
+          // presets: ['react', 'es2015']
+        // }
+      // }
+    // ]
+  // },
+  // resolve: {
+    // extensions: ['.js', '.es6']
+  // },
+  // plugins: [HTMLWebpackPluginConfig]
+};
+// eof
