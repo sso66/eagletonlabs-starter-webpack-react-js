f57e9de88ad3dd08d0951a64509b3520f97dd347
sso66 <sun.stephen.oo@gmail.com>
2018-03-07 11:51:56 -0500

    update configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -14,31 +14,31 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/public'
   }, 
-  watch: true,
-  // module: {
-    // loaders: [
-      // {
-        // test: /\.jshint$/,
-        // exclude: /node_modules/,
-        // loader: 'jshint-loader',
-        // query: {
-          // cacheDirectory: true,  
-          // presets: ['react', 'es2015']
-        // }
-      // },  
-      // {
-        // test: [/\.js$/, /\.es6$/],
-        // exclude: /node_modules/,
-        // loader: 'babel-loader',
-        // query: {
-          // presets: ['react', 'es2015']
-        // }
-      // }
-    // ]
-  // },
-  // resolve: {
-    // extensions: ['.js', '.es6']
-  // },
-  // plugins: [HTMLWebpackPluginConfig]
+  //watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jshint$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader',
+        query: {
+          cacheDirectory: true,  
+          presets: ['react', 'es2015']
+        }
+      },  
+      {
+        test: [/\.js$/, /\.es6$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+    resolve: {
+    extensions: ['.js', '.es6']
+  },
+  //plugins: [HTMLWebpackPluginConfig] 
 };
 // eof
