9acc425761b26dd7b8bcf2495838b0d840e058c9
sso66 <sun.stephen.oo@gmail.com>
2018-08-05 07:47:59 -0400

    clean up eagleton web designs

--- public/index.node.js
+++
@@ -1,6 +0,0 @@
-// index.node.js
-console.log("Starting index.node.js...");
-
-var greeter = require('./components/Greeter.node.js');
-document.getElementById('root').appendChild(greeter());
-// eof 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
 // webpack.config.js
-console.info('Mounting webpack.config.js...');
+console.info( 'Mounting webpack.config.js...' );
 
 module.exports = {
-  entry: __dirname + '/src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/build'
-  }, 
-  //watch: true,
-  module: {
-    rules: [
-      {
-        // test: /\.jshint$/,
-        // exclude: /node_modules/,
-        // loader: 'jshint-loader',
-        // query: {
-          // cacheDirectory: true,  
-          // presets: ['react', 'es2015']
-        // }
-      },  
-      {
-        test: [/\.js$/, /\.es6$/, /\.jsx$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  },
-    resolve: {
-    extensions: ['.js', '.es6', '.jsx']
-  },
+	entry: __dirname + '/src/app.js',
+	output: {
+		filename: 'bundle.js',
+    	path: __dirname + '/build'
+  	},
+  	//watch: true,
+	module: {
+		rules: [
+      		{
+				// test: /\.jshint$/,
+				// exclude: /node_modules/,
+				// loader: 'jshint-loader',
+				// query: {
+				// cacheDirectory: true,
+				// presets: ['react', 'es2015']
+				// }
+	      	},
+			{
+				test: [ /\.js$/, /\.es6$/, /\.jsx$/ ],
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: [ 'react', 'es2015' ]
+				}
+      		}
+		]
+ 	},
+	resolve: {
+		extensions: [ '.js', '.es6', '.jsx' ]
+	},
 };
+
 // eof
+
