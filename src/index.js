02678c8dcba101b562e717358e0acf400adf77dc
sso66 <sun.stephen.oo@gmail.com>
2019-07-21 19:34:33 -0400

    clean up public library

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ console.info('Mounting index.js ... ');
 
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-
 import './utils/logger.es6';
 import './redux/traffic-light';
-
 import Root from './containers/Root';
 import configureStore from './redux/configureStore';
-
 import './styles/index.sass';
 
 const {store, actions} = configureStore();
@@ -26,7 +23,7 @@ store.subscribe(init);
 try {
     window.onload = init;
 } catch(e) {
-    alert("Engleton End" + e.message);
+    alert("Engleton Web Designs" + e.message);
     e.preventDefault();
 } finally {
     console.log("Application mounted successfully!");
