c006ba2f4792908cff2a9e4619fcfbecc7df72f7
Sun Stephen Oo <sun.stephen.oo@gmail.com>
2020-09-15 14:23:05 -0400

    Update configureStore.js

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -4,7 +4,7 @@
 // ................................................................................
 console.log( "Mounting src/redux/store/configureStore.js..." );
 
-// "store": STATE
+// "store": STATE - Diner
 //........................................
 import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/rootReducer';
